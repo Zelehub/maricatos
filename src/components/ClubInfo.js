0098e13358d7751014b23189f8f1a62f9dd57d81
Jose Oliveira <jedpfo89@gmail.com>
2025-06-23 10:05:48 +0100

    basic content

---
+++ src/components/ClubInfo.js
@@ -0,0 +1,13 @@
+
+
+export default function ClubInfo() {
+
+
+
+  return (
+    <div className="info-container">
+      <div className="coach-container"></div>
+      <div className="schedule-container"></div>
+    </div>
+  )
+}
---
+++ src/components/Mission.js
@@ -0,0 +1,15 @@
+export default function Mission() {
+  return (
+    <div className="mission-container">
+      <div className="mission-content">
+        <div>
+          <h3>Our Mission</h3>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis sequi ipsam nemo accusamus magni quibusdam suscipit consequatur? Eum ex minima inventore iusto quisquam nobis voluptatem unde saepe tenetur molestias! Deleniti.</p>
+        </div>
+        <div>
+          <img src="https://renzogracieacademy.com/wp-content/uploads/2019/09/BJJ-Master-Renzo-Gracie-780x1024.jpg" alt="imagem" style={{height: 200, width: 200}}></img>
+        </div>
+      </div>
+    </div>
+  );
+}
